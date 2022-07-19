cc09b08d23de4a40e429ee21ccddbfe6adae7eeb
Moringa <alinoorshukri8@gmail.com>
2022-07-19 09:36:09 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import Toggle from "./Toggle";
 function App() {
   return (
     <div>
-      <h3>Toggle</h3>
-      <Toggle />
+        <h3>Toggle</h3>
+        <Toggle />
     </div>
   );
 }
--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  return  <button>OFF</button>;
 }
 
 export default Toggle;
